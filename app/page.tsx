5ba216cb57b6fe591f3d2a00d2461eddfe3ec4e1
PendingBrew <brewpending@gmail.com>
2026-04-18 01:41:31 +0800

    feat: create design nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import { Logo, ThemeIcon, MenuIcon } from "./assets";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello Bro!</h1>
+      {/* Navigation Menus */}
+      <nav className="fixed w-full flex justify-between p-3 md:p-10">
+        <Image src={Logo} alt="Logo" className="w-[90px]" />
+
+        <div className="flex gap-5 md:gap-8">
+          <Image
+            src={ThemeIcon}
+            alt="Theme Icon"
+            className="scale-100 md:scale-125"
+          />
+          <Image
+            src={MenuIcon}
+            alt="Menu Icon"
+            className="scale-100 md:scale-125"
+          />
+        </div>
+      </nav>
     </>
   );
 }
